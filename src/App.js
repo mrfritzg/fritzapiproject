aaedfccd647fc6b8d02d9058b8a7ef9637b1c6bd
mrfritzg <mrfritz@gamil.com>
2023-02-28 19:29:33 -0500

    added useEffects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
 function App() {
   return (
-    <div className="App">
+    <div>
+
+      let key = process.env.REACT_APP_KEY;
+
+      let {symbol} = useParams(); // -> {symbol: ''}
+
+      console.log(symbol);
+
+      let url = `http://rest-sandbox.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${key}`;
+
+      let [ coin, setCoin ] = useState(null);
+
+{/* let navigate = useNavigate() //->returns a function */}
+
+      async function getData() {
+    try {
+        const response = await fetch(url);
+      const data = await response.json();
+      setCoin(data);
+    } catch (err) {
+        console.error(err);
+    }
+}
       
+      useEffect( () => {
+        getData()
+      })
+
+
     </div>
   );
 }
