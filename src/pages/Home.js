c3154093148f6cb19f4076624ced0c72e3842563
mrfritzg <mrfritz@gamil.com>
2023-06-27 01:07:47 -0400

    handled fetch errors for home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import CharacterList from "../components/CharacterList";
 
 export default function Home() {
-  
+
   // let key = process.env.REACT_APP_KEY;
 
   // let {symbol} = useParams(); // -> {symbol: ''}
@@ -11,35 +11,44 @@ export default function Home() {
 
   let url = `https://akabab.github.io/superhero-api/api/all.json`;
 
-  let [ heroData, setHeroData ] = useState([]);
+  let [heroData, setHeroData] = useState([]);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null)
 
-{/* let navigate = useNavigate() //->returns a function */}
+  {/* let navigate = useNavigate() //->returns a function */ }
 
   async function getHeroData() {
-try {
-    const response = await fetch(url);
-  const data = await response.json();
-  setHeroData(data);
-  setIsPending(false);
-} catch (err) {
-    console.error(err);
-}
-}
-  
-  useEffect( () => {
+    try {
+      const response = await fetch(url);
+      // console.log(response);
+      if (!response.ok) {
+        throw Error('ERROR!! -- could not reach the data for that resource');
+      }
+      const data = await response.json();
+      setHeroData(data);
+      setIsPending(false);
+      setError(null);
+    } catch (err) {
+      console.error(err);
+      setError(err.message);
+      setIsPending(false)
+    }
+  }
+
+  useEffect(() => {
     getHeroData()
   }, [])
-//  console.log(heroData);
+  //  console.log(heroData);
   return (
     <div className="homepage">
 
-     <h1>The Super Heros/Villian Website</h1>
-     <h2>This Website provides Heros/Villian from mutliple universes. Most Websites give you only once comic book creator source, but we give you Marvel, DC and more!.</h2>
-     <h2>Please Scroll Thru and find what you're looking for or Click on the Search Page.</h2>
+      <h1>The Super Heros/Villian Website</h1>
+      <h2>This Website provides Heros/Villian from mutliple universes. Most Websites give you only once comic book creator source, but we give you Marvel, DC and more!.</h2>
+      <h2>Please Scroll Thru and find what you're looking for or Click on the Search Page.</h2>
 
-     {isPending && <div><h1>LOADING...</h1></div> }
-     <CharacterList heroData={heroData}/>
+      {error && <div><h1>{error}</h1></div>}
+      {isPending && <div><h1>LOADING...</h1></div>}
+      <CharacterList heroData={heroData} />
 
     </div>
   );
